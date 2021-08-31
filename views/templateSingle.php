da288f22f300dac5446e3acc91594f3924aed713
elgato <amzfba.1bestbuy@gmail.com>
2021-08-31 21:00:09 +0200

    Added some templates post and detailled post

---
+++ views/templateSingle.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Vue Article</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<!-- Font -->
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link href="public/common-css/bootstrap.css" rel="stylesheet">
+	<link href="public/common-css/ionicons.css" rel="stylesheet">
+	<link href="public/single-post-1/css/styles.css" rel="stylesheet">
+	<link href="public/single-post-1/css/responsive.css" rel="stylesheet">
+
+</head>
+<body >
+	<header>
+		<div class="container-fluid position-relative no-side-padding">
+			<a href="#" class="logo"><img src="public/images/logo.png" alt="Logo Image"></a>
+			<div class="menu-nav-icon" data-nav-menu="#main-menu"><i class="ion-navicon"></i></div>
+
+			<?php include("public/partials/_nav.php"); ?>
+
+		</div><!-- conatiner -->
+	</header>
+
+
+<?=$content?> <!-- viewSinglePost.php -->
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-section">
+						<a class="logo" href="#"><img src="public/images/logo.png" alt="Logo Image"></a>
+						<p class="copyright">Mettre twig date </p>
+						<ul class="icons">
+							<li><a href="#"><i class="ion-social-facebook-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-twitter-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-vimeo-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-pinterest-outline"></i></a></li>
+						</ul>
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
+
+				<div class="col-lg-4 col-md-6">
+						<div class="footer-section">
+
+					</div><!-- footer-section -->
+
+				</div><!-- col-lg-4 col-md-6 -->
+
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-section">
+
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
+
+			</div><!-- row -->
+		</div><!-- container -->
+	</footer>
+
+
+	<!-- SCIPTS -->
+
+	<script src="public/common-js/jquery-3.1.1.min.js"></script>
+
+	<script src="public/common-js/tether.min.js"></script>
+
+	<script src="public/common-js/bootstrap.js"></script>
+
+	<script src="public/common-js/scripts.js"></script>
+
+</body>
+</html>

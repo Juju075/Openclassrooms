39fb5b20ae40a74bfa1c843ec268ae9134cf1297
elgato <amzfba.1bestbuy@gmail.com>
2021-08-31 19:29:11 +0200

    My first commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * http://localhost/App_Blog_MVC/index.php?url=accueil
+ * https://github.com/Juju075/Openclassrooms.git
  */
 require_once('framework/Router.php');
 $router = new Router(); 

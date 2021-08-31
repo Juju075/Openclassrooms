daad43664ec995615af851dcfa457d2d776491cf
elgato <amzfba.1bestbuy@gmail.com>
2021-08-31 21:07:48 +0200

    Right configuration of composer.json

---
+++ public/partials/_footer.php
@@ -0,0 +1,25 @@
+<div class="col-lg-4 col-md-6">
+					<div class="footer-section">
+
+						<a class="logo" href="#"><img src="public/images/logo.png" alt="Logo Image"></a>
+						<p class="copyright">Bona @ 2017. All rights reserved.</p>
+						<p class="copyright">Designed by </p>
+						<ul class="icons">
+							<li><a href="#"><i class="ion-social-facebook-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-twitter-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-vimeo-outline"></i></a></li>
+							<li><a href="#"><i class="ion-social-pinterest-outline"></i></a></li>
+						</ul>
+
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
+
+				 
+				<div class="col-lg-4 col-md-6">
+					<div class="footer-section">
+
+
+
+					</div><!-- footer-section -->
+				</div><!-- col-lg-4 col-md-6 -->
---
+++ public/partials/_nav.php
@@ -0,0 +1,14 @@
+			<ul class="main-menu visible-on-click" id="main-menu">
+				<!-- Twig -->
+				<li><a href="accueil">Accueil</a></li>
+				<li><a href="contact&create">Nous contacter</a></li>
+				
+				<!-- Twig si non connecte -->
+				<li><a href="register&create">S'inscrire</a></li>
+				<li><a href="login&user">S'authentifier</a></li>
+
+				
+				<!-- Twig si connecte-->
+				<li><a href="post&create">Créer un article</a></li>
+				
+			</ul><!-- main-menu -->

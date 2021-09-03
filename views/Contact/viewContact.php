20ceb812259d779a27b331e3e577990bdd1798b3
elgato <amzfba.1bestbuy@gmail.com>
2021-09-03 15:33:11 +0200

    Added some files in Contact and form folders

---
+++ views/Contact/viewContact.php
@@ -0,0 +1,56 @@
+<div class="container-contact100">
+		<div class="wrap-contact100">
+
+			<form  method="post" action="contact&status=new" class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Envoyer un message à l'Administrateur.
+				</span>
+
+				<div class="wrap-input100 validate-input" data-validate="Title is required">
+					<span class="label-input100">Titre</span>
+					<input class="input100" type="text" name="prenom" placeholder="Entrez un titre">
+					<span class="focus-input100"></span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate="Title is required">
+					<span class="label-input100">Titre</span>
+					<input class="input100" type="text" name="nom" placeholder="Entrez un titre">
+					<span class="focus-input100"></span>
+                </div>
+				<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<span class="label-input100">Email</span>
+					<input class="input100" type="text" name="email" placeholder="Enter your email addess">
+					<span class="focus-input100"></span>
+				</div>
+                <div class="wrap-input100 validate-input" data-validate="Title is required">
+					<span class="label-input100">Titre</span>
+					<input class="input100" type="text" name="mot de passe" placeholder="Entrez un titre">
+					<span class="focus-input100"></span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate="Title is required">
+					<span class="label-input100">Titre</span>
+					<input class="input100" type="text" name="nom" placeholder="Entrez un titre">
+					<span class="focus-input100"></span>
+                </div>
+                
+
+
+				<div class="wrap-input100 validate-input" data-validate = "Contenu is required">
+					<span class="label-input100">Article</span>
+					<textarea class="input100" name="content" placeholder="Votre texte ici..."></textarea>
+					<span class="focus-input100"></span>
+				</div>
+
+				<div class="container-contact100-form-btn">
+					<div class="wrap-contact100-form-btn">
+						<div class="contact100-form-bgbtn"></div>
+						<button class="contact100-form-btn">
+							<span>
+								Soumettre
+								<i class="fa fa-long-arrow-right m-l-7" aria-hidden="true"></i>
+							</span>
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>

109f81f07f070a94cc2330155cc4562a279b8151
elgato <amzfba.1bestbuy@gmail.com>
2021-09-04 21:06:20 +0200

    Working on intro path

--- controllers/ControllerIntro.php
+++ controllers/ControllerIntro.php
@@ -6,5 +6,26 @@ require_once('models/Manager/ArticleManager.php');
  */
  class ControllerIntro
  {
+    private $_articleManager;
+    private $_view; 
+
+     public function __construct(){
+         echo('fonction constructeur ok');
+
+         if(isset($url) && count($url) > 1){ 
+            throw new \Exception("Page introuvable", 1);
+        } 
+        else{
+            $this->presentation();  
+        }       
+     }
+
+     //Affiche un contenu simple pas de repository
+     private function presentation(){
+
+        $this->_view = new View('intro', 'Intro');
+
+        $this->_view->displayForm('Intro'); 
+     }
 
 }
